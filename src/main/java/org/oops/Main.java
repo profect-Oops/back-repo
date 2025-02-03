672eada0d415a69f5d8649786047a75a458ea35b
MJLee39 <dlsdjdmlqlc3@naver.com>
2025-02-03 21:32:36 +0900

    Initial commit

---
+++ src/main/java/org/oops/Main.java
@@ -0,0 +1,11 @@
+package org.oops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // 스프링 부트 애플리케이션으로 설정
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args); // 스프링 부트 실행
+    }
+}
