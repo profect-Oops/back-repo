6bd978c994c738b5dabbc2131d30152bfa86565b
MJLee39 <dlsdjdmlqlc3@naver.com>
2025-02-05 14:52:33 +0900

    코인 리스트 조회

---
+++ src/main/java/org/oops/api/coin/dto/CoinDTO.java
@@ -0,0 +1,45 @@
+package org.oops.api.coin.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.oops.domain.coin.Coin;
+
+import javax.swing.text.html.parser.Entity;
+import java.io.Serializable;
+
+@Getter
+public class CoinDTO implements Serializable {
+
+    private final Long coinId;
+
+    private final String name;
+
+    private final Float prospects;
+
+    private final String picture;
+
+    private final Boolean isVisible;
+
+    private final String ticker;
+
+    @Builder
+    public CoinDTO(Long coinId, String name, Float prospects, String picture, Boolean isVisible, String ticker){
+        this.coinId = coinId;
+        this.name = name;
+        this.prospects = prospects;
+        this.picture = picture;
+        this.isVisible = isVisible;
+        this.ticker = ticker;
+    }
+
+    public static CoinDTO fromEnity(Coin coin){
+        return CoinDTO.builder()
+                .coinId(coin.getCoinId())
+                .name(coin.getName())
+                .prospects(coin.getProspects())
+                .picture(coin.getPicture())
+                .isVisible(coin.getIsVisible())
+                .ticker(coin.getTicker())
+                .build();
+    }
+}
