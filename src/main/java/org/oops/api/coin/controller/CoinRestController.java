97cb9e6c384527948a165b3bbe6e9a6e030333de
MJLee39 <dlsdjdmlqlc3@naver.com>
2025-02-05 14:52:33 +0900

    코인 리스트 조회

---
+++ src/main/java/org/oops/api/coin/controller/CoinRestController.java
@@ -0,0 +1,27 @@
+package org.oops.api.coin.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.oops.api.coin.dto.CoinDTO;
+import org.oops.api.coin.service.CoinService;
+import org.oops.api.common.controller.BaseController;
+import org.oops.api.common.dto.ResponseDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/coin")
+public class CoinRestController extends BaseController {
+
+    private final CoinService coinService;
+
+    @GetMapping(value = "/read")
+    public ResponseDTO<List<CoinDTO>> getCoins(){
+        return ResponseDTO.ok(coinService.getVisibleCoins());
+    }
+
+}
---
+++ src/main/java/org/oops/domain/coin/Coin.java
@@ -0,0 +1,66 @@
+package org.oops.domain.coin;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@DynamicUpdate
+@NoArgsConstructor
+public class Coin {
+
+    @Id
+    @Column(name = "COIN_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long coinId;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "PROSPECTS")
+    private Float prospects;
+
+    @Column(name = "COIN_PICTURE")
+    private String picture;
+
+    @Column(name = "ISVisible", nullable = false)
+    @ColumnDefault("false")
+    private Boolean isVisible;
+
+    @Column(name = "TICKER")
+    private String ticker;
+
+    @Builder
+    public Coin(String name, Float prospects, String picture, Boolean isVisible, String ticker) {
+        this.name = name;
+        this.prospects = prospects;
+        this.picture = picture;
+        this.isVisible = isVisible;
+        this.ticker = ticker;
+    }
+
+    public static final Coin fromDTO(final String name, final Float prospects, final String picture, final Boolean isVisible, final String ticker) {
+        return Coin.builder()
+                .name(name)
+                .prospects(prospects)
+                .picture(picture)
+                .isVisible(isVisible)
+                .ticker(ticker)
+                .build();
+    }
+
+    public void updateIsVisible(Boolean isVisible) {
+        this.isVisible = isVisible;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        if (this.isVisible == null) {
+            this.isVisible = false;
+        }
+    }
+}
